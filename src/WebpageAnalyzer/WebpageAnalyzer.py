a42c6695fa771fc5276189acd142a902ff4ebb81
Filip Pietraszak <filippiet@gmail.com>
2019-04-16 15:51:54 +0200

    test

--- src/WebpageAnalyzer/WebpageAnalyzer.py
+++ src/WebpageAnalyzer/WebpageAnalyzer.py
@@ -1,8 +1,11 @@
+import requests
+
+
 class WebpageAnalyzer:
     def __init__(self):
         pass
 
-    def get_images(self, webpage, location, min_threshold, max_threshold):
+    def get_images(self, webpage_source, location, min_threshold, max_threshold):
         """
         Analyzes given webpage and downloads all images meeting given requirements
         :param webpage: URL to the site to download images from
@@ -14,7 +17,7 @@ class WebpageAnalyzer:
 
         raise NotImplementedError
 
-    def get_urls_with_description(self, webpage, location=None):
+    def get_urls_with_description(self, webpage_source, location=None):
         """
         Analyzes given webpage and returns list of tuples with links and descriptions found,
         optionally saves obtained data to the file
@@ -25,6 +28,19 @@ class WebpageAnalyzer:
 
         raise NotImplementedError
 
+    @staticmethod
+    def get_webpage_source(webpage_url):
+        """
+        Returns source of given webpage or raises exception if page not accessible
+        :param webpage_url: URL to obtain source code from
+        :return: string of raw source code
+        """
+        response = requests.get(webpage_url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            raise Exception(f"Source not obtained, response status code: [{response.status_code}]")
+
 
 if __name__ == "__main__":
-    pass
+    print(WebpageAnalyzer.get_webpage_source("https://www.pyszne.pl/"))
