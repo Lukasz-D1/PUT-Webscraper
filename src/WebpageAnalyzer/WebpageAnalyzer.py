1b66deeb65a9cc5412b403a139418b440bba1e6c
Filip Pietraszak <filippiet@gmail.com>
2019-05-28 20:11:12 +0200

    Scrap subpages working

--- src/WebpageAnalyzer/WebpageAnalyzer.py
+++ src/WebpageAnalyzer/WebpageAnalyzer.py
@@ -166,13 +166,13 @@ class WebpageAnalyzer:
             for key, value in output.copy().items():
                 if value == 0:
                     try:
-                        subpages = self.get_urls_with_description(key, file_location)
+                        subpages, _ = self.get_urls_with_description(key, file_location)
                         for subpage in subpages:
                             if subpage[0] not in output.keys():
                                 output[subpage[0]] = 0
                                 print(".", end='')
-                    except:
-                        print("smth")
+                    except Exception as e:
+                        print("e")
                 output[key] = value + 1
         from pprint import pprint
         pprint(output)
@@ -181,13 +181,14 @@ class WebpageAnalyzer:
 
 if __name__ == "__main__":
     anal = WebpageAnalyzer()
+    #
+    # websites_list = ["http://www.pyszne.pl", "http://fee.put.poznan.pl/index.php/en/", "http://wykop.pl"]
+    #
+    # #images = anal.get_images("http://wykop.pl", "images/",10000,100000)
+    # urls,ilosc = anal.scrap_multiple_websites(websites_list, "file.txt")
+    #
+    # print(ilosc)
 
-    websites_list = ["http://www.pyszne.pl", "http://fee.put.poznan.pl/index.php/en/", "http://wykop.pl"]
+    print(anal.scrap_subpages(1, "http://info.cern.ch/"))
 
-    #images = anal.get_images("http://wykop.pl", "images/",10000,100000)
-    urls,ilosc = anal.scrap_multiple_websites(websites_list, "file.txt")
-
-    print(ilosc)
-
-   # anal.scrap_subpages(2, "http://www.michalwolski.pl/")
-    anal.get_urls_with_description("http://www.pyszne.pl")
+    #anal.get_urls_with_description("http://www.pyszne.pl")
